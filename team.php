90e7aabcbaf3dc668715b4b82e5de498611d1e2d
chri_mc_tech <chrimctech@gmail.com>
2026-07-06 00:42:35 +0200

    wip

--- team.php
+++ team.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html>
 <html lang="it">
 
-<head>
-    <meta charset="UTF-8">
-
-    <title>ChriSystems</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
-
-    <link rel="stylesheet" href="style.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-</head>
+<?php
+include 'includes/head.php';
+?>
 
 <body>
 
